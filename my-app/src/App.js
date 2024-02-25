c2e329de1b72a1f85ff8c980856be32f787dbfaf
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2024-02-25 20:51:36 +0100

    hw_74

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,13 @@
-
 import React from 'react';
 import Card from './Card';
 
 function App() {
     return (
         <div className="App">
-            <Card />
+            <Card title="First Card" />
+            <Card text="How are you?" />
+            <Card title="Third Card" text="Hello, how are you?" />
         </div>
     );
 }
-
 export default App;
