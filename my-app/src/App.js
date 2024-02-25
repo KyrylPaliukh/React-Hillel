6fe093f2461f3940a277b9dc28d6a88a5a632b89
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2024-02-25 20:42:49 +0100

    hw_73

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,11 @@
+
 import React from 'react';
-import './App.css';
+import Card from './Card';
 
 function App() {
     return (
         <div className="App">
-            <header>
-                <h1>Тест заголовок</h1>
-            </header>
-            <div className="container">
-                <nav>
-                    <p>Тест бокова навігація</p>
-                </nav>
-                <main>
-                    <div>
-                        <p>Тест центральний контент</p>
-                    </div>
-                </main>
-            </div>
+            <Card />
         </div>
     );
 }
