87a3b6cfcfa8d09dac55f80712ec43333ad421fe
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2024-02-25 20:27:46 +0100

    hw_72

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header>
+                <h1>Тест заголовок</h1>
+            </header>
+            <div className="container">
+                <nav>
+                    <p>Тест бокова навігація</p>
+                </nav>
+                <main>
+                    <div>
+                        <p>Тест центральний контент</p>
+                    </div>
+                </main>
+            </div>
+        </div>
+    );
 }
 
 export default App;
