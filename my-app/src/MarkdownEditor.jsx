740bb17e00ba30908f1c71d91e5264635a9ed91f
Kyryl Paliukh <paliukh.kyryl@gmail.com>
2024-03-17 14:37:51 +0100

    hw_85

---
+++ my-app/src/MarkdownEditor.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useRef } from 'react';
+import Editor from '@toast-ui/editor';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
+const MarkdownEditor = ({ onContentChange }) => {
+    const editorRef = useRef(null);
+
+    useEffect(() => {
+        const editor = new Editor({
+            el: editorRef.current,
+            hideModeSwitch: true,
+        });
+
+        editor.addHook('change', () => {
+            const content = editor.getMarkdown();
+            onContentChange(content);
+        });
+
+        return () => {
+            editor.remove();
+        };
+    }, [onContentChange]);
+
+    return <div ref={editorRef} />;
+};
+
+export default MarkdownEditor;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import '@toast-ui/editor/dist/toastui-editor.css';
+
 import ReactDOM from 'react-dom';
-import MyForm from './MyForm';
+import React from 'react';
+
+import MarkdownEditor from './MarkdownEditor.jsx';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <MyForm />
-    </React.StrictMode>,
-    document.getElementById('root')
+    <MarkdownEditor onContentChange={console.log} />,
+    document.getElementById('container'),
 );
